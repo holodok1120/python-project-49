5b893df355043e5e0037d0f3b35a106574d9ad90
holodok1120 <holodok1120@gmail.com>
2025-10-22 10:07:27 +0300

    Correct style code of game brain-progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -10,11 +10,11 @@ def get_question() -> str:
     start_value = random.randint(0, 25)
     step = random.randint(1, 10)
     progression_lenght = random.randint(10, 10)
-    for index in range (progression_lenght):
+    for index in range(progression_lenght):
         current_element = start_value + index * step
         current_element = str(current_element)
         progression_list.append(current_element)
-    replacement_index = random.randint(0, progression_lenght-1)
+    replacement_index = random.randint(0, progression_lenght - 1)
     progression_list[replacement_index] = ".."
     result = " ".join(progression_list)
     return result
@@ -24,11 +24,11 @@ def get_correct_answer(question: str) -> str:
     question_list = question.split()
     required_index = question_list.index("..")
     if required_index + 2 > len(question_list):
-        major_number = question_list[required_index-1]
-        minor_number = question_list[required_index-2]
+        major_number = question_list[required_index - 1]
+        minor_number = question_list[required_index - 2]
     else:
-        major_number = question_list[required_index+1]
-        minor_number = question_list[required_index+2]
+        major_number = question_list[required_index + 1]
+        minor_number = question_list[required_index + 2]
     major_number = int(major_number)
     minor_number = int(minor_number)
     step = major_number - minor_number
